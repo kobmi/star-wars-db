46cbd7bb4d204e0fa4d868ef4edf627615611d56
Sergey <sergeykolesnichenko@ukr.net>
2020-12-10 16:36:09 +0200

    added error-boudries

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,54 +4,60 @@ import Header from "../header";
 import RandomPlanet from "../random-planet";
 import ItemList from "../item-list";
 import PersonDetails from "../person-details";
+import ErrorButton from "../error-button";
+import ErrorIndicator from "../error-indicator";
 
 import "./app.css";
+import PeoplePage from "../people-page";
 
 class App extends Component {
-    state = {
-        showRandomPlanet: true,
-        selectedItem: null,
-    };
+  state = {
+    showRandomPlanet: true,
+    hasError: false,
+  };
 
-    toogleShowRandomPlanet = () => {
-        this.setState((prevState) => {
-            return {
-                showRandomPlanet: !prevState.showRandomPlanet,
-            };
-        });
-    };
+  componentDidCatch() {
+    console.log("didCatch");
+    this.setState({ hasError: true });
+  }
+  toogleShowRandomPlanet = () => {
+    this.setState((prevState) => {
+      return {
+        showRandomPlanet: !prevState.showRandomPlanet,
+      };
+    });
+  };
 
-    onItemSelected = (id) => {
-        this.setState({ selectedItem: id });
-    };
-
-    render() {
-        const { showPlanet, selectedItem } = this.state;
-        const buttonTitle = showPlanet
-            ? "Hide Random planet"
-            : "Show Random Planet";
-        const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
-        return (
-            <div className="app">
-                <Header />
-                {planet}
-                <button
-                    className="toggle-planet btn btn-warning btn-lg"
-                    onClick={this.toogleShowRandomPlanet}
-                >
-                    {buttonTitle}
-                </button>
-                <div className="row mb2">
-                    <div className="col-md-6">
-                        <ItemList onItemSelected={this.onItemSelected} />
-                    </div>
-                    <div className="col-md-6">
-                        <PersonDetails itemId={selectedItem} />
-                    </div>
-                </div>
-            </div>
-        );
+  render() {
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
     }
+    const { showPlanet } = this.state;
+    const buttonTitle = showPlanet
+      ? "Hide Random planet"
+      : "Show Random Planet";
+    const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
+    return (
+      <div className="container">
+        <div className="stardb-app">
+          <Header />
+          {planet}
+          <div className="row mb2 button-row">
+            <button
+              className="toggle-planet btn btn-warning btn-lg"
+              onClick={this.toogleShowRandomPlanet}
+            >
+              {buttonTitle}
+            </button>
+            <ErrorButton />
+          </div>
+          <PeoplePage />
+          <PeoplePage />
+          <PeoplePage />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
